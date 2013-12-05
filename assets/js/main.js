00cd208ce7cd33bc4758ac1940d9f8e1deb0b8d2
Sun Liwen <sunliwen@gmail.com>
2013-12-05 10:41:24 +0800

    Add info box fields and batch alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,37 @@
 Handlebars.registerHelper('table', function(items, options) {
   var out = "<table>";
 
-  //console.log(items);
   for(var i in items) {
-    out = out + "<tr>";
     item = items[i];
-    //console.log(item);
 
     for(var key in item){
-      out = out + "<th style='text-align: right'><span>" + key + "</span></th>";
-      out = out + "<td style='word-break: break-word;'><span>" + item[key] + "</span></td>";
+      if(item[key] !== ""){
+        out = out + "<tr>";
+        out = out + "<th style='text-align: right'><span>" + key + "</span></th>";
+        out = out + "<td style='word-break: break-word;'><span>" + item[key] + "</span></td>";
+        out = out + "</tr>";
+      }
     }
-
-    out = out + "</tr>";
   }
+
+  return out + "</table>";
+});
+
+Handlebars.registerHelper('batches-table', function(items, options) {
+  var out = "<table>";
+  out = out + "<tr>";
+  out = out + "<th style='text-align: right'><span>StreamItem</span></th>";
+  out = out + "<td style='word-break: break-word;'><span>Count</span></td>";
+  out = out + "</tr>";
+
+  $.each(items, function(k, v){
+    //console.log(k);
+    //console.log(v);
+    out = out + "<tr>";
+    out = out + "<th style='text-align: right'><span style='font-size: 60%'>" + k + "</span></th>";
+    out = out + "<td style='word-break: break-word;'><span>" + v + "</span></td>";
+    out = out + "</tr>";
+  });
 
   return out + "</table>";
 });
